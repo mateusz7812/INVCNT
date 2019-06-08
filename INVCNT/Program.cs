aa17497ccc11f7bc3f1468a665f14232dc03ecb0
mateusz7812 <mateusz78122@gmail.com>
2019-06-08 09:07:48 +0200

    Dodaj pliki projektów.

---
+++ INVCNT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace INVCNT
+{
+    class Program
+    {
+        static void Main()
+        {
+            int testsQuantity = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            for (int i = 0;i < testsQuantity; i++)
+            {
+                int listQuantity = Convert.ToInt32(Console.ReadLine());
+                List<int> list = new List<int>();
+                for (int j = 0; j < listQuantity; j++)
+                    list.Add(Convert.ToInt32(Console.ReadLine()));
+                int result = GetInversionsQuantity(list);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+        }
+
+        private static int GetInversionsQuantity(List<int> list)
+        {
+            int InversionQuantity = 0;
+            for (int i = 0; i < list.Count(); i++)
+                for (int j = i; j < list.Count(); j++)
+                {
+                    if (i == j)
+                        continue;
+                    if (list[i] > list[j])
+                        InversionQuantity++;
+                }
+            return InversionQuantity;
+        }
+    }
+}
